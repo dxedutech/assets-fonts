6ef08d8b5bbedca3031523d9466ac0a87334a33d
myramyun <oloccip@naver.com>
2026-06-06 20:23:29 +0900

    20260606

--- woof/home/home.js
+++ woof/home/home.js
@@ -1,4 +1,26 @@
 export function init() {
-  console.log("홈 페이지 로직 실행 1");
-  // 홈 관련 이벤트 등록 등
+	console.log('홈 페이지 로직 실행 Home');
 }
+
+function getParentUserID() {
+	try {
+		const userElement = window.parent.document.querySelector('#userID');
+
+		if (userElement) {
+			const userID = userElement.innerText;
+			console.log('부모에서 가져온 아이디:', userID);
+
+			localStorage.setItem('loggedInUser', userID);
+			return userID;
+		} else {
+			console.warn("부모 페이지에서 'userID' 요소를 찾을 수 없습니다.");
+		}
+	} catch (error) {
+		console.error(
+			'보안 정책(CORS)으로 인해 부모 페이지에 접근할 수 없습니다.',
+			error,
+		);
+	}
+}
+
+window.addEventListener('load', getParentUserID);
--- woof/tangram/tangram.js
+++ woof/tangram/tangram.js
@@ -1,4 +1,3 @@
 export function init() {
-  console.log("홈 페이지 로직 실행");
-  // 홈 관련 이벤트 등록 등
+  console.log("홈 페이지 로직 실행 Tangram");
 }
