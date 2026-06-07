c545f64ae7e1cd095cb9ab30d419390aba160eba
myramyun <oloccip@naver.com>
2026-06-07 11:43:28 +0900

    260607

--- woof/home/home.js
+++ woof/home/home.js
@@ -1,4 +1,17 @@
+import { dx } from '../../wore/hex.js';
+
 export function init() {
 	console.log('홈 페이지 로직 실행 Home');
 }
 
+document.querySelectorAll('.hex a').forEach((a) => {
+	a.addEventListener('click', (e) => {
+		e.preventDefault();
+		const page = new URL(a.href).searchParams.get('page');
+		console.log(page);
+		if (page) {
+			// window.history.pushState({ page }, '', `?page=${page}`);
+			dx.loadPage({ p: page });
+		}
+	});
+});
