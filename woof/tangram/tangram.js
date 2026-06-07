3494e877976aa60f85748cc033f3a96f655caa21
myramyun <oloccip@naver.com>
2026-06-07 19:47:08 +0900

    260607

--- woof/tangram/tangram.js
+++ woof/tangram/tangram.js
@@ -1,17 +1,5 @@
-import { dx } from '../../wore/hex.js';
+// import { dx } from '../../wore/hex.js';
 
 export function init() {
-	console.log('홈 페이지 로직 실행 Home');
+	console.log('홈 페이지 로직 실행 Tangram');
 }
-
-document.querySelectorAll('nav a').forEach((a) => {
-	a.addEventListener('click', (e) => {
-		e.preventDefault();
-		const page = new URL(a.href).searchParams.get('page');
-		console.log(page);
-		if (page) {
-			// window.history.pushState({ page }, '', `?page=${page}`);
-			dx.loadPage({ p: page });
-		}
-	});
-});
--- wore/hex.js
+++ wore/hex.js
@@ -5,10 +5,12 @@ const main = document.querySelector('main');
 const loadPage = async (v) => {
 	const { p } = v;
 
+	/*** Html 제어 */
 	v.res = await fetch(`${basePath}/woof/${p}/${p}.html`);
 	v.html = await v.res.text();
 	main.innerHTML = v.html;
 
+	/*** 로컬과 서버 이미지 호출 주소 제어 */
 	main.querySelectorAll('img').forEach(img => {
 		if (!img.src.includes(basePath) && !img.src.startsWith('http')) {
 			v.src = img.getAttribute('src');
@@ -16,12 +18,25 @@ const loadPage = async (v) => {
 		}
 	});
 
+	/*** Javascript 제어 */
 	try {
 		v.m = await import(`${basePath}/woof/${p}/${p}.js`);
-		if (v.m.init) v.m.init();
+		if (v.m.init) v.m.init(); /// 해당 자바스크립트를 가져와 해당 함수 실행
 	} catch (e) {
 		console.log('이 페이지는 별도 JS가 필요 없습니다.');
 	}
 };
 
-export const dx = { loadPage, basePath };
+/*** Nav a 태그 라우터 제어 */
+main.addEventListener('click', (e) => {
+  const a = e.target.closest('a');
+  if (!a) return;
+
+  e.preventDefault();
+  const page = new URL(a.href).searchParams.get('page');
+  if (page) {
+    loadPage({ p: page });
+  }
+});
+
+export const dx = { loadPage };
